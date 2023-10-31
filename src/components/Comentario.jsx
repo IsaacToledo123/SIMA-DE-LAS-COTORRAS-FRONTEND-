77883507eab6787fc2e7cdc39be1724edf8078ee
Christopher Yahir <223209@ids.upchiapas.edu.mx>
2023-10-31 12:53:22 -0600

    Responsive página principal

--- src/components/Comentario.jsx
+++ src/components/Comentario.jsx
@@ -4,11 +4,11 @@ function Comentario({ username, userphoto, comment, date }) {
 
             <div className="p-10">
 
-                  <div className="p-10 flex justify-center">
+                  <div className="md:p-5 p-10 flex justify-center font-bold">
                         <div>
                               <h1 className="text-xl">{username}</h1>
-                              <p className="text-green-600 font-bold">{date}</p>
-                              <img src={userphoto} alt="Foto del usuario" width={100} />
+                              <p className="text-green-600 font-bold pt-2">{date}</p>
+                              <img src={userphoto} alt="Foto del usuario pt-2" width={100} />
 
                         </div>
 
