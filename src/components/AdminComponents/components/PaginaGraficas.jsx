36ddc3257d2e6c73118c7af6dbf49e0cfdeabea6
SaraMabTauro <144514203+SaraMabTauro@users.noreply.github.com>
2023-11-05 12:13:42 -0600

    Componente de Graficas

--- src/components/AdminComponents/components/PaginaGraficas.jsx
+++ src/components/AdminComponents/components/PaginaGraficas.jsx
@@ -1,74 +1,98 @@
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartPie } from '@fortawesome/free-solid-svg-icons';
-import { PieChart, Pie, BarChart, Bar, Cell, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { PieChart, Pie, BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
-const GraficasView = () => {
-  const [chartType, setChartType] = useState('ingresos');
+const GraficosSeccionados = () => {
 
-  
-  const datosIngresos = [
-    { name: 'Cocina', monto: 500 },
-    { name: 'Aventuras', monto: 300 },
-    { name: 'Otros', monto: 200 },
+  // Datos de ejemplo
+  const dataEgresosCategorias = [
+    { name: 'Categoría 1', value: 1000 },
+    { name: 'Categoría 2', value: 2000 },
+    { name: 'Categoría 3', value: 1500 },
   ];
 
-  const datosEgresos = [
-    { name: 'Alquiler', monto: 800 },
-    { name: 'Compras', monto: 200 },
-    { name: 'Otros', monto: 300 },
+  const dataEgresosDia = [
+    { name: 'Enero', value: 200 },
+    { name: 'Febrero', value: 150 },
+    { name: 'Marzo', value: 300 },
+    { name: 'Abril', value: 250 },
+    { name: 'Mayo', value: 180 },
+  ];
+
+  const dataEgresoMayorFlujo = [
+    { name: 'Egreso 1', value: 500 },
+    { name: 'Egreso 2', value: 800 },
+  ];
+
+  
+  const dataIngresos = [
+    { name: 'Enero', value: 2000 },
+    { name: 'Febrero', value: 1500 },
+    { name: 'Marzo', value: 3000 },
+    { name: 'Abril', value: 2500 },
+    { name: 'Mayo', value: 1800 },
+  ];
+
+  
+  const dataReservas = [
+    { name: 'Reserva 1', value: 500 },
+    { name: 'Reserva 2', value: 800 },
   ];
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-20 h-20 bg-red-500 rounded-full flex items-center justify-center text-white">
-        <FontAwesomeIcon icon={faChartPie} size="3x" />
+    <div className="grid grid-cols-2 gap-4">
+      <h1 className="text-4xl uppercase font-bold text-indigo-700 text-center mb-4">
+        RESERVAS
+      </h1>
+      <div className="border p-4 rounded-lg bg-white">
+        <h2 className="text-xl font-semibold mb-4">Egresos por Categorías</h2>
+        <PieChart width={400} height={300}>
+          <Pie dataKey="value" data={dataEgresosCategorias} cx="50%" cy="50%" outerRadius={80} fill="#6495ED" label />
+          <Tooltip />
+        </PieChart>
       </div>
-      <span className="mt-2 text-xl">Gráficas</span>
-      <div className="mt-4 space-x-4">
-        <button
-          className={`${
-            chartType === 'ingresos' ? 'bg-green-500' : 'bg-gray-300'
-          } text-white px-4 py-2 rounded hover:bg-green-600 cursor-pointer`}
-          onClick={() => setChartType('ingresos')}
-        >
-          Ingresos
-        </button>
-        <button
-          className={`${
-            chartType === 'egresos' ? 'bg-red-500' : 'bg-gray-300'
-          } text-white px-4 py-2 rounded hover:bg-red-600 cursor-pointer`}
-          onClick={() => setChartType('egresos')}
-        >
-          Egresos
-        </button>
+
+      <div className="border p-4 rounded-lg bg-white">
+        <h2 className="text-xl font-semibold mb-4">Egresos al Día</h2>
+        <BarChart width={400} height={300} data={dataEgresosDia}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="#00008B" />
+        </BarChart>
       </div>
-      <div className="mt-4" style={{ width: '80%' }}>
-        {chartType === 'ingresos' ? (
-          <PieChart width={400} height={400}>
-            <Pie data={datosIngresos} dataKey="monto" nameKey="name" cx="50%" cy="50%" outerRadius={80} fill="#8884d8">
-              {datosIngresos.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={entry.color} />
-              ))}
-            </Pie>
-            <Tooltip />
-            <Legend />
-          </PieChart>
-        ) : (
-          <BarChart width={400} height={400} data={datosEgresos}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="monto" fill="#82ca9d" />
-          </BarChart>
-        )}
+
+      <div className="border p-4 rounded-lg bg-white">
+        <h2 className="text-xl font-semibold mb-4">Egreso con Mayor Flujo</h2>
+        <PieChart width={400} height={300}>
+          <Pie dataKey="value" data={dataEgresoMayorFlujo} cx="50%" cy="50%" outerRadius={80} fill="#8884d8" label />
+          <Tooltip />
+        </PieChart>
       </div>
+
+      <div className="border p-4 rounded-lg bg-white">
+        <h2 className="text-xl font-semibold mb-4">Ingresos</h2>
+        <BarChart width={400} height={300} data={dataIngresos}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="#8B008B" />
+        </BarChart>
+      </div>
+
+      <div className="border p-4 rounded-lg bg-white">
+        <h2 className="text-xl font-semibold mb-4">Reservas</h2>
+        <PieChart width={400} height={300}>
+          <Pie dataKey="value" data={dataReservas} cx="50%" cy="50%" outerRadius={80} fill="#5F9EA0" label />
+          <Tooltip />
+        </PieChart>
+      </div>
+
     </div>
   );
 };
 
-export default GraficasView;
+export default GraficosSeccionados;
 
 
 
