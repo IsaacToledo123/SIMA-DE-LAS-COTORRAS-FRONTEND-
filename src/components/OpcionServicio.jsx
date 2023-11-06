1078f6c8ca0b4d1eb42aaa1961a2e9d7a1846e86
Christopher Yahir <223209@ids.upchiapas.edu.mx>
2023-11-06 12:38:43 -0600

    Estoy cansado jefe

--- src/components/OpcionServicio.jsx
+++ src/components/OpcionServicio.jsx
@@ -4,18 +4,20 @@ import Añadir from "../img/añadir.png";
 import CabñaInfo from "./CabñaInfo";
 
 function OpcionServicio({ aventura, verServicio}) {
-  const [mostrar, setMostrar] = useState(false);
 
       return (
 
             <div className="flex items-center pb-10" onClick={e => {verServicio(aventura)}}>
+
                   <div className="bg-gray-200 pl-4 h-10 w-60 text-center p-1">{aventura}</div>
+
                   <div>
-                        <img src={Añadir} className="h-10 w-10 ml-4 hover:animate-spin hover:cursor-pointer"/>
+                        <img src={Añadir} className="h-10 w-10 ml-4 hover:animate-spin hover:cursor-pointer"/>                        
                   </div>
+
             </div>
 
-      )
+      )     
 }
 
 export default OpcionServicio;
