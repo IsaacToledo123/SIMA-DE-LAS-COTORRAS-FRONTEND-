7415cb8e42f6e96d7cace5fcce3016a3d933f80a
SaraMabTauro <144514203+SaraMabTauro@users.noreply.github.com>
2023-11-08 22:51:13 -0600

    Agregacion de Iconos a Menu

--- src/components/AdminComponents/components/Menu.jsx
+++ src/components/AdminComponents/components/Menu.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+/*import React from 'react';
 import Graficas from '../../../img/graficas.png';
 import Egresos from '../../../img/egresos.png';
 import Ingresos from '../../../img/ingresos.png';
-import Metas from '../../../img/metas.png';
+import Metas from '../../../img/metas.png';*/
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartBar, faMoneyBillAlt, faMoneyCheckAlt} from '@fortawesome/free-solid-svg-icons';
+
 const ColoredSections = () => {
   return (
     <div className="flex flex-col w-screen h-screen bg-gray-700">
       <div className="h-1/4 bg-red-400 mb-3 mt-3 rounded-lg flex items-center">
         <div className="w-1/3 p-4">
-          <img src={Graficas} alt="Imagen" className="w-2/4 h-auto mx-auto rounded-lg" />
+            <FontAwesomeIcon icon={faChartBar} className="text-8xl text-white" />
         </div>
         <div className="w-2/3 h-full bg-red-600 p-4 rounded-xl">
           <div className='flex flex-col items-center justify-center'>
@@ -21,7 +24,7 @@ const ColoredSections = () => {
 
       <div className="h-1/4 bg-blue-400 mb-3 mt-3 rounded-lg flex items-center">
         <div className="w-1/3 p-4">
-          <img src={Ingresos} alt="Imagen Azul" className="w-2/4 h-auto mx-auto rounded-lg" />
+            <FontAwesomeIcon icon={faMoneyBillAlt} className="text-8xl text-white" />
         </div>
         <div className="w-2/3 h-full bg-blue-600 p-4 rounded-xl">
           <div className='flex flex-col items-center justify-center'>
@@ -34,7 +37,7 @@ const ColoredSections = () => {
 
       <div className="h-1/4 bg-green-400 mb-3 mt-3 rounded-lg flex items-center">
         <div className="w-1/3 p-4">
-          <img src={Egresos} alt="Imagen Verde" className="w-2/4 h-auto mx-auto rounded-lg" />
+            <FontAwesomeIcon icon={faMoneyCheckAlt} className="text-8xl text-white" />
         </div>
         <div className="w-2/3 h-full bg-green-600 p-4 rounded-xl">
           <div className='flex flex-col items-center justify-center'>
@@ -45,18 +48,6 @@ const ColoredSections = () => {
         </div>
       </div>
 
-      <div className="h-1/4 bg-yellow-400 mb-3 mt-3 rounded-lg flex items-center">
-        <div className="w-1/3 p-4">
-          <img src={Metas} alt="Imagen Amarilla" className="w-2/5 h-auto mx-auto rounded-lg" />
-        </div>
-        <div className="w-2/3 h-full bg-yellow-600 p-4 rounded-xl">
-          <div className='flex flex-col items-center justify-center'>
-            <h1 className="text-5xl p-2">Metas</h1>
-            <p className='text-2xl pb-5'>Define el futuro de tu empresa</p>
-            <button className='bg-gray-200 p-2 px-5 rounded-lg'><a href="/ruta-amarilla">IR</a> </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
