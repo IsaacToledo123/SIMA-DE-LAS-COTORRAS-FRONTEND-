96cd9c0200858f63e603e8d6d487ebf920157100
SaraMabTauro <144514203+SaraMabTauro@users.noreply.github.com>
2023-11-08 22:29:23 -0600

    Ajuste en el componente de Graficas

--- src/components/AdminComponents/components/PaginaGraficas.jsx
+++ src/components/AdminComponents/components/PaginaGraficas.jsx
@@ -22,7 +22,6 @@ const GraficosSeccionados = () => {
     { name: 'Egreso 2', value: 800 },
   ];
 
-  
   const dataIngresos = [
     { name: 'Enero', value: 2000 },
     { name: 'Febrero', value: 1500 },
@@ -31,7 +30,6 @@ const GraficosSeccionados = () => {
     { name: 'Mayo', value: 1800 },
   ];
 
-  
   const dataReservas = [
     { name: 'Reserva 1', value: 500 },
     { name: 'Reserva 2', value: 800 },
@@ -39,9 +37,12 @@ const GraficosSeccionados = () => {
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      <h1 className="text-4xl uppercase font-bold text-indigo-700 text-center mb-4">
-        RESERVAS
-      </h1>
+      <div className="col-span-2">
+        <h1 className="text-4xl uppercase font-bold text-indigo-700 text-center mb-4">
+          RESERVAS
+        </h1>
+      </div>
+
       <div className="border p-4 rounded-lg bg-white">
         <h2 className="text-xl font-semibold mb-4">Egresos por Categorías</h2>
         <PieChart width={400} height={300}>
@@ -81,13 +82,25 @@ const GraficosSeccionados = () => {
       </div>
 
       <div className="border p-4 rounded-lg bg-white">
-        <h2 className="text-xl font-semibold mb-4">Reservas</h2>
-        <PieChart width={400} height={300}>
+        <div className="border p-4 rounded-lg bg-white">
+          <h2 className="text-xl font-semibold mb-4">Reservas</h2>
+          <PieChart width={400} height={300}>
           <Pie dataKey="value" data={dataReservas} cx="50%" cy="50%" outerRadius={80} fill="#5F9EA0" label />
           <Tooltip />
         </PieChart>
+        </div>
       </div>
 
+      <div className="border p-4 rounded-lg bg-white">
+        <h2 className="text-xl font-semibold mb-4">Egresos al Día</h2>
+        <BarChart width={400} height={300} data={dataEgresosDia}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="#ADD8E6" />
+        </BarChart>
+      </div>
     </div>
   );
 };
