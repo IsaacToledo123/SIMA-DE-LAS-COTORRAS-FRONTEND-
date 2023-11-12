f98b657eeda3abdfd32066ab13e5c9d3a0eed3b6
IsaacToledo123 <223245@ids.upchiapas.edu.mx>
2023-11-11 18:01:06 -0600

    corquis

--- src/components/croquis.jsx
+++ src/components/croquis.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import Croquis from '../img/fotografias_cabaña/croquis.png';
-
+import Croquis from '../img/imagenesCRoquis/croquis.png'
+import Rapel from '../img/imagenesCRoquis/rapel.jpg'
 const CroquisComponent = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <div className="max-w-xl mx-4">
-        <img src={Croquis} alt="Croquis" className="w-full h-auto rounded-lg" />
-        {/* Aquí puedes agregar contenido adicional si es necesario */}
-      </div>
+    <div className="relative w-full h-screen flex items-center justify-center">
+    <div className="max-w-xl mx-4 relative">
+      <img src={Croquis} alt="Croquis" className="w-full h-auto rounded-lg" />
+      <img src={Rapel} alt="Rapel" className="absolute top-80 right-44 w-16 rounded-lg cursor-pointer hover:shadow-2xl " />
+
     </div>
+  </div>
   );
 };
 
