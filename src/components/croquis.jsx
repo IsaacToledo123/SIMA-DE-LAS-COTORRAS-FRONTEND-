597b07d0c326b0eb1c5c7cce7bab96497cc0a086
IsaacToledo123 <223245@ids.upchiapas.edu.mx>
2023-11-12 21:44:34 -0600

    corquis

--- src/components/croquis.jsx
+++ src/components/croquis.jsx
@@ -5,13 +5,11 @@ import Restaurant from '../img/imagenesCRoquis/local.png'
 const CroquisComponent = () => {
   return (
     <div className="relative flex items-center justify-center ">
-      
-    <div className="max-w-xl mx-4 relative">
+    <div className="max-w-3xl mx-4 relative">
       <img src={Croquis} alt="Croquis" className="w-full h-auto rounded-lg" />
       <img src={Rapel} alt="Rapel" className="absolute top-80 right-44 w-16 rounded-lg cursor-pointer hover:border-2 hover:border-yellow-500 transform hover:scale-110 transition-transform duration-300 ease-in-out" />
       <img src={Restaurant} alt="Rapel"  className="absolute top-36 right-32 w-16 rounded-lg cursor-pointer hover:border-4 hover:border-yellow-500 transform hover:scale-110 transition-transform duration-300 ease-in-out"
- />
-
+ /> 
     </div>
   </div>
   );
--- src/components/ubicacion.jsx
+++ src/components/ubicacion.jsx
@@ -1,36 +1,58 @@
 import React from "react";
 import CroquisComponent from "./croquis";
-import FondoMapa from '../img/imagenesCRoquis/fondoMapa.png'
-import Rapel from '../img/imagenesCRoquis/rapel.jpg'
-import Restaurant from '../img/imagenesCRoquis/local.png'
+import FondoMapa from "../img/imagenesCRoquis/fondoMapa.png";
+import Rapel from "../img/imagenesCRoquis/rapel.jpg";
+import Restaurant from "../img/imagenesCRoquis/local.png";
+import LogoEmpresa from "../img/logoP.png";
 const Ubicacion = () => {
   return (
-    <div className=" flex " >
-  <div class=" top-0 left-0 h-auto w-1/8 bg-green-400  flex flex-col text-center ">
-    <div className="pb-10 pt-5">
-        <h1 class="text-2xl font-semibold p-10">Simbología</h1>
-    </div>
-    <div className="p-10 text-2xl hover:border border-white">
-  <p class="">Cabañas</p>
-</div>
-<div className="p-10 text-2xl flex   hover:border border-white  ">
-<img src={Rapel} className="w-10 pr-2" />
-  <p class="">Rapel</p>
- 
-</div>
-<div className="p-10 text-2xl hover:border border-white">
-  <p class="">Restaurant</p>
-</div>
-<div className="p-10 text-2xl hover:border border-white">
-  <p class="">Entrada</p>
-</div>
+    <div className="flex p-10">
+      {/* Componente Izquierdo */}
+      <div className="w-1/4 m-0">
+        <div className="text-2xl font-semibold flex flex-col text-center">
+          <h1 className="">La sima de las cotorras</h1>
+          <h2>Mapa turístico</h2>
+        </div>
+        <p className="text-justify text-2xl p-10">
+          La Sima de las Cotorras es un fascinante destino turístico que combina
+          la emoción de la naturaleza con comodidades modernas. Situada en un
+          entorno natural impresionante, esta joya mexicana ofrece una
+          experiencia única para los visitantes en busca de aventuras auténticas
+          y relajación.
+        </p>
+      </div>
 
-</div>
-<div className="flex ml-auto w-3/4 p-40  ">
-<CroquisComponent/>
-</div>
+      {/* Componente Central (CroquisComponent) */}
+      <div className="flex-1">
+        <CroquisComponent />
+      </div>
+
+      {/* Componente Derecho */}
+      <div className="w-1/4">
+        <div>
+          <img src={LogoEmpresa} width="300px" />
+        </div>
+       
+        <div className="flex flex-col p-10">
+          <div className="flex pb-5">
+            <img src={Rapel} className="w-10" />
+            <h1 className="text-2xl pl-5 cursor-pointer hover:text-yellow-200"><a href="/reservas">Rapel</a></h1>
+          </div>
+          <div className="flex pb-5">
+            <img src={Restaurant} className="w-10" />
+            <h1 className="text-2xl pl-5 cursor-pointer hover:text-yellow-200">Restaurant</h1>
+          </div>
+          <div className="flex pb-5">
+            <img src={Rapel} className="w-10" />
+            <h1 className="text-2xl pl-5 cursor-pointer hover:text-yellow-200"><a href="/reservas">Cabañas</a></h1>
+          </div>
+          <div className="flex pb-5">
+            <img src={Rapel} className="w-10" />
+            <h1 className="text-2xl pl-5 cursor-pointer hover:text-yellow-200"><a >Usted esta aqui</a></h1>
+          </div>
+        </div>
+      </div>
     </div>
-    
   );
 };
 
